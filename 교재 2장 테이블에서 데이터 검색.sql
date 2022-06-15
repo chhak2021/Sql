18e5a5b49075c78f88309c4f1c6b6cfda18039cd
chhak2021@gmail.com <chhak2021@gmail.com>
2022-06-15 17:01:40 +0900

    2022-06-15 수업예제

--- 교재 2장 테이블에서 데이터 검색.sql
+++ 교재 2장 테이블에서 데이터 검색.sql
@@ -24,7 +24,19 @@ INSERT INTO `sample21` (`name`, `address`) VALUES ('홍길동', '서울특별시
 #p73
 
 # 테이블 생성/데이터 저장
-CREATE TABLE `sample24` ();
+CREATE TABLE `sample24` (
+	`no` INT AUTO_INCREMENT PRIMARY KEY,
+	`a` INT,
+	`b` INT,
+	`c` INT 
+);
+
+INSERT INTO `sample24` (`a`, `b`, `c`) VALUES (1, 0, 0);
+INSERT INTO `sample24` (`a`, `b`, `c`) VALUES (0, 1, 0);
+INSERT INTO `sample24` (`a`, `b`, `c`) VALUES (0, 0, 1);
+INSERT INTO `sample24` (`a`, `b`, `c`) VALUES (2, 2, 0);
+INSERT INTO `sample24` (`a`, `b`, `c`) VALUES (0, 2, 2);
+
 #p76
 #p77
 #p78
@@ -32,7 +44,16 @@ CREATE TABLE `sample24` ();
 #p82
 
 # 테이블 생성/데이터 저장
-CREATE TABLE `sample25` ();
+CREATE TABLE `sample25` (
+	`no` INT AUTO_INCREMENT PRIMARY KEY,
+	`text` VARCHAR(255)
+);
+
+INSERT INTO `sample25` SET `text` = 'SQL은 RDBMS를 조작하는 언어이다';
+INSERT INTO `sample25` SET `text` = 'LIKE에서는 메타문자 %와 _를 사용할 수 있다';
+INSERT INTO `sample25` SET `text` = 'LIKE는 SQL에서 사용할 수 있는 술어 중 하나이다';
+
+
 #p85
 #p86
 #p88
